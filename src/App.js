0eb135eecd3f1011d9cc7f4a56d3d7948d07432a
guptanie <niket267@gmail.com>
2020-07-22 03:41:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       return;
     }
  
-    axios.get('http://localhost:4000/verifyToken?token=${token}').then(response => {
+    axios.get('http://53d3f7bbdc8c.ngrok.io/verifyToken?token=${token}').then(response => {
       setUserSession(response.data.token, response.data.user);
       setAuthLoading(false);
     }).catch(error => {
